b4ea81ddaae6fab3b24689a43cc7057645a0d736
Ana.S Rosero <1081055280@u.icesi.edu.co>
2026-05-14 03:39:36 -0500

    Add changes

--- SameBeat-3/src/App.tsx
+++ SameBeat-3/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from './screens/Home/HomeScreen';
-import Discover from './screens/Discover/DiscoverScreen';
+ import Discover from './screens/Discover/DiscoverScreen';
 import Concerts from './screens/Concerts/Concerts';
 import ConcertDetails from './screens/ConcertDetails/ConcertDetails';
 import ConcertDescription from './screens/ConcertDescription/ConcertDescription';
@@ -11,11 +11,12 @@ import { FilterProvider } from './contexts/FilterContext';
 import ChatScreen from './screens/ChatScreens/ChatScreen';
 import MapScreen from './screens/MapScreen/MapScreen';
 import MatchScreen from './screens/MatchScreen/MatchScreen';
+
+
 import OnboardingScreen from './screens/OnboardingScreen/OnboardingScreen';
 import SignUpScreen from './screens/SignUpScreen/SignUpScreen';
 import LoginScreen from './screens/LoginScreen/LoginScreen';
 import ConcertGenres from './screens/GenresScreen/ConcertGenres';
-
 
 export default function App() {
 
@@ -24,7 +25,6 @@ export default function App() {
     <FilterProvider>
 
       <Router>
-
 
         <Routes>
 
@@ -112,30 +112,6 @@ export default function App() {
           />
 
         </Routes>
-
-        <MainLayout>
-          <Routes> 
-            <Route path="/home" element={<HomeScreen />} />
-            <Route path='/discover' element={<Discover/>}/>
-            <Route path="/concerts" element={<Concerts />} />
-            <Route path="/concert/:id" element={<ConcertDetails />} />
-            <Route path="/concert/:id/description" element={<ConcertDescription />} />
-            <Route path="/concert/:id/communities" element={<Communities />} />
-            <Route path="/discover" element={<Discover />} />
-            <Route path="/profile" element={<ProfileScreen />} />
-            <Route path="/chat" element={<ChatScreen />} />
-            <Route path="/map" element={<MapScreen />} />
-            <Route path="/match" element={<MatchScreen />} />
-            <Route path="/create/post" element={<div>POST</div>} />
-            <Route path="/create/song" element={<div>SONG</div>} />
-            <Route path="/create/video" element={<div>VIDEO</div>} />
-            <Route path="/create/photo" element={<div>PHOTO</div>} />
-
-            <Route path="/Chat" element={<ChatScreen />} />
-            <Route path="/Map" element={<MapScreen />} />
-            <Route path="/Match" element={<MatchScreen />} />
-          </Routes>
-        </MainLayout>
 
       </Router>
 
